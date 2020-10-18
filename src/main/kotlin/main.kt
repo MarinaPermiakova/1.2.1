d7c7a528d8f731b7139d591ac54a340487a7b185
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-10-18 08:43:57 +0400

    fixed errors

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,8 @@
 fun main() {
-    val amountRub = 10000.50
-    val amountKop = (amountRub * 100).toInt()
+    val amount = 1_500_000
     val commissionInPercent = 0.0075
-    val maxCommission = 3500
-    val commission = (amountKop * commissionInPercent).toInt()
-    val result = if (commission > maxCommission) maxCommission/100.0 else commission/100.0
-    println("Total commission: $result rub.")
+    val minCommission = 3_500
+    val commission = (amount * commissionInPercent).toInt()
+    val result = if (commission > minCommission) commission else minCommission
+    println("Total commission: $result kop.")
 }
